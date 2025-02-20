9df516300e87c206144767d3dc1e19defe0c2454
Asif-GD <asif.coach@gmail.com>
2025-02-20 16:06:55 +0530

    setup a index view that renders a index.html listing the latest 5 questions - also setup dummy detail, results, vote views.

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /polls/
     path("", views.index, name="index"),
+    # /polls/5/
+    path("<int:question_id>/", views.get_question_details, name="question_details"),
+    # /polls/5/results/
+    path("<int:question_id>/results/", views.get_question_results, name="question_results"),
+    # /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote_question, name="vote_question"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from polls.models import Question
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello Asif! You're at the polls view.")
+    latest_question_list: [list] = Question.objects.order_by("pub_date")[:5]
+    # context is a dict() that holds the python variables which is sent to the template
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return render(request, template_name="polls/index.html", context=context)
+
+
+def get_question_details(request, question_id):
+    return HttpResponse(f"You're viewing question {question_id}")
+
+
+def get_question_results(request, question_id):
+    return HttpResponse(f"You're viewing results of question {question_id}")
+
+
+def vote_question(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
