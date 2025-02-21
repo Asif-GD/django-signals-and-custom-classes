d641c1a1af025358401c87d9fa7344678861b769
Asif-GD <asif.coach@gmail.com>
2025-02-21 13:16:28 +0530

    add endpoint to view question details and choices

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 
 from polls.models import Question
 
@@ -7,15 +7,32 @@ from polls.models import Question
 # Create your views here.
 def index(request):
     latest_question_list: [list] = Question.objects.order_by("pub_date")[:5]
+
     # context is a dict() that holds the python variables which is sent to the template
     context = {
         "latest_question_list": latest_question_list,
     }
+
     return render(request, template_name="polls/index.html", context=context)
 
 
 def get_question_details(request, question_id):
-    return HttpResponse(f"You're viewing question {question_id}")
+
+    """
+    # this can be rewritten as
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist!")
+    """
+    question = get_object_or_404(Question, pk=question_id)
+
+    # context is a dict() that holds the python variables which is sent to the template
+    context = {
+        "question": question,
+    }
+
+    return render(request, template_name="polls/detail.html", context=context)
 
 
 def get_question_results(request, question_id):
